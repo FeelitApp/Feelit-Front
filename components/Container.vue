64e484c526a1b79f8e6b7ccc847894fe926fd078
alice darondeau <alicedarondeau@MacBook-Air-de-alice-2.local>
2023-11-03 17:19:40 +0100

    :poop: Wip custom fonts

---
+++ components/Container.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-white rounded-xl border-2 max-w-[1300px]">
+
+    <slot/>
+
+  </div>
+</template>
---
+++ components/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-[url('/bg_hero.svg')] bg-no-repeat bg-cover">
+    <div class="flex flex-col lg:flex-row gap-8 justify-center items-center px-10 py-6 lg:px-20 lg:py-10">
+
+      <img src="/logo_feelit_green.svg" class="w-52 object-contain"/>
+
+      <div class="text-black font-grotesk text-2xl lg:text-4xl flex flex-col gap-2 font-semibold ">
+        <h1 class="bg-white px-1 py-1 w-fit">
+          Apprenez à identifier
+        </h1>
+        <h1 class="bg-white px-1 py-1">
+          et décoder vos émotions
+        </h1>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
