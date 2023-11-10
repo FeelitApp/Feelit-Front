23b7ec12fcf7508ebc2ceefce5ea62847cdd2991
Theo Seuge <theoseuge@Neey.local>
2023-11-10 16:35:26 +0100

    :art: Improve card components

--- components/Card.vue
+++ components/Card.vue
@@ -1,7 +1,11 @@
 <template>
-    <div :style="`background-color: ${props.color}`" class="rounded-xl border-2 border-black w-64 overflow-hidden shadow-[4px_4px_0_rgba(0,0,0,1)] pt-3 pb-5 px-6">
+    <div 
+        :style="`background-color: ${props.color}`" 
+        v-bind:class="[props.button ? 'w-[48rem] flex-row items-center pt-3 pb-3 gap-4' : 'w-64 flex-col pt-3 pb-5']"
+        class="rounded-xl border-2 border-black overflow-hidden shadow-[4px_4px_0_rgba(0,0,0,1)] flex px-6">
         <h1 class="text-3xl font-grotesk font-semibold">{{ props.title }}</h1>
         <p class="text-lg font-grotesk font-extralight leading-5">{{ props.content }}</p>
+        <slot />
     </div>
 </template>
 
@@ -17,6 +21,9 @@ const props = defineProps({
     },
     content: {
         type: String,
+    },
+    button: {
+        type: Boolean,
     }
 })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -10,12 +10,22 @@ const route = useRoute()
         :color="'#FACDEB'" 
         :title="'Sensations'"
         :content="'Lorem ipsum dolor sit amet, consec tetur adipiscing elit.'"
+        :button="false"
       />
       <Button
         :color="'#FFFFF'"
         :content="'En savoir plus'"
         :icon="'/arrow.svg'"
       />
+      <Card 
+        :color="'#FACDEB'" 
+        :content="'Lorem ipsum dolor sit amet, consec tetur adipiscing elit.'"
+        :button="true"
+      ><Button
+        :color="'#FFFFF'"
+        :content="'En savoir plus'"
+        :icon="'/arrow.svg'"
+      /></Card>
     </Container>
   </div>
 </template>
