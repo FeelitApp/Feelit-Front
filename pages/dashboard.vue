6ed762a91ee978a9dc063380114624a885be096d
justine <justine.rougeulle@agence-modo.fr>
2024-07-03 18:45:15 +0200

    update champs modification infos

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -57,7 +57,7 @@ async function update() {
 
 onMounted(() => {
   if (!("geolocation" in navigator)) {
-    errorStr.value = 'Geolocation pas disponible';
+    errorStr.value = 'Géolocalisation indisponible';
     return;
   }
 
@@ -70,6 +70,7 @@ onMounted(() => {
     const url = `http://api.weatherapi.com/v1/current.json?key=49043bfb42f6476388b134930231512&q=${pos.coords.latitude},${pos.coords.longitude}&aqi=no`;
 
     const data = await $fetch(url);
+    data.current.temp_c = Math.round(data.current.temp_c);
     meteoData.value = data
   }, err => {
     gettingLocation.value = false;
@@ -93,7 +94,7 @@ onMounted(() => {
           </div>
           <div class="flex sm:flex-row gap-6 sm:gap-10 sm:w-[45%]">
             <div class="w-1/2 h-auto items-center gap-0 rounded-xl border-2 border-black overflow-hidden shadow-[4px_4px_0_rgba(0,0,0,1)] flex flex-col px-6 py-6">
-              <span class="text-5xl font-grotesk text-purple">{{currDay}}</span>
+              <span class="text-5xl font-grotesk">{{currDay}}</span>
               <span class="text-xl font-grotesk">{{currMonth}}</span>
               <span class="text-3xl font-grotesk">{{currYear}}</span>
             </div>
@@ -101,7 +102,7 @@ onMounted(() => {
               <div class="flex flex-row justify-between">
                 <span class="font-grotesk" v-if="errorStr">Impossible d'obtenir la localisation</span>
                 <span class="font-grotesk" v-if="meteoData">{{ meteoData.location.name }}</span>
-                <span class="font-bold font-grotesk text-pink" v-if="meteoData">{{ meteoData.current.temp_c }}°C</span>
+                <span class="font-bold font-grotesk" v-if="meteoData">{{ meteoData.current.temp_c }}°C</span>
               </div>
               <div class="h-8">
                 <img class="object-cover w-48" v-if="meteoData" :src="meteoData.current.condition.icon" alt="icon_meteo">
@@ -148,43 +149,61 @@ onMounted(() => {
           <div class="w-full px-4 py-2 mt-0 border-b-2 border-black bg-lime">
             <p class="font-grotesk">Paramètres du compte</p>
           </div>
-          <div class="flex flex-col justify-center gap-6 px-6 py-6 sm:px-10 sm:py-8 sm:gap-10">
+          <div class="flex flex-col justify-center gap-6 px-6 py-10 sm:px-10 sm:py-8 sm:gap-10">
             <div class="flex flex-col gap-6 sm:grid sm:grid-cols-2 sm:gap-8">
-              <Input
-                  :type="'username'"
-                  :name="'username'"
-                  :label="'Nom d\'utilisateur :'"
-                  v-model="account.data.username"
-              />
-              <Input
-                  :type="'email'"
-                  :name="'email'"
-                  :label="'E-mail :'"
-                  v-model="account.data.email"
-              />
-              <Input
-                  :type="'password'"
-                  :name="'password'"
-                  :label="'Nouveau mot de passe :'"
-              />
-              <Input
-                  :type="'password'"
-                  :name="'confirmPassword'"
-                  :label="'Confirmer le mot de passe :'"
-              />
+                <Input
+                    :type="'username'"
+                    :name="'username'"
+                    :label="'Nom d\'utilisateur :'"
+                    v-model="account.data.username"
+                />
+                <Input
+                    :type="'email'"
+                    :name="'email'"
+                    :label="'E-mail :'"
+                    v-model="account.data.email"
+                />
+              </div>
               <span class="text-green-400 font-grotesk" v-if="isUpdated">Les modifications ont bien été enregistrées.</span>
               <span class="text-red-400 font-grotesk" v-if="errorUpdate.username">{{ errorUpdate.username.at(0) }}</span>
               <span class="text-red-400 font-grotesk" v-if="errorUpdate.email">{{ errorUpdate.email.at(0) }}</span>
+              <div class="mx-auto">
+                <Button
+                    class="text-md"
+                    :color="'#FFFFFF'"
+                    :content="'Enregistrer les modifications'"
+                    @click="update"
+                />
+              </div>
+              <Input
+                      :type="'password'"
+                      :name="'currentPassword'"
+                      :label="'Mot de passe actuel :'"
+                  />
+            <div class="flex flex-col gap-2 sm:grid sm:grid-cols-2 sm:gap-8">
+              <Input
+                    :type="'password'"
+                    :name="'newPassword'"
+                    :label="'Nouveau mot de passe :'"
+                />
+                <Input
+                    :type="'password'"
+                    :name="'confirmPassword'"
+                    :label="'Confirmer le mot de passe :'"
+                />
+                <span class="text-green-400 font-grotesk" v-if="isUpdated">Mot de passe modifié.</span>
+                <span class="text-red-400 font-grotesk" v-if="errorUpdate.username">{{ errorUpdate.username.at(0) }}</span>
+                <span class="text-red-400 font-grotesk" v-if="errorUpdate.email">{{ errorUpdate.email.at(0) }}</span>
             </div>
             <div class="mx-auto">
               <Button
                   class="text-md"
                   :color="'#FFFFFF'"
-                  :content="'Enregistrer les modifications'"
+                  :content="'Modifier le mot de passe'"
                   @click="update"
               />
             </div>
-          </div>
+            </div>
         </div>
 
         <div class="flex flex-col items-center gap-6 sm:flex-row sm:justify-center sm:gap-12">
